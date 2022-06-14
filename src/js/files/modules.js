08111b8edffbc8b4a5239160c4ab823193502c9b
Dima Tytenko <amgpetronass@gmail.com>
2022-06-14 11:07:44 +0300

    add

--- src/js/files/modules.js
+++ src/js/files/modules.js
@@ -1,1 +1,6 @@
-export const flsModules = {}
+export const flsModules = {};
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
